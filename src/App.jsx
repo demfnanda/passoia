22bb643afbe883500e720a0b05c0dd375d0c1906
demfnanda <fernanda_menezes72@hotmail.com>
2025-11-12 13:36:35 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// AQUI IMPORTAMOS OS ARQUIVOS
+import Header from "./components/Header"
+
+// Arquivo Principal
+// para escrita do nome do componente funcional usamos sempre a primeira letra maiuscula
+
+function App(){
+  return(
+     <>
+     <Header/>
+     </>
+  )
+}
+
+export default App 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+
+
+function Header (){
+    return(
+        <header>
+            <img src="" alt="logo da marca da pagina" />
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADE</li>
+            </ul>
+        </header>
+
+    )
+}
